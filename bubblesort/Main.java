05dca04efc5dc30b75859fd031a267da17dbac55
Andrii Dzhuhan <andzhuhan@gmail.com>
2024-07-19 17:53:05 +0200

    Bubblesort XD

---
+++ bubblesort/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Bubblesort {
+    static void sort(int a[])
+    {
+        int temp;
+        for(int i = 0; i < a.length; i++) {
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        int a[] = {2345, 2, 345, 23, 123, 33, 11, 1};
+        Bubblesort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
